8cbb5aac24dbe09d24ac615233b9253c28cc4a5c
Lnight <94696816+EvgenyPlaksin@users.noreply.github.com>
2022-09-27 23:18:22 +0300

    add binding to MainActivity.kt

--- app/src/main/java/com/example/a45myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/a45myapplication/presentation/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.a45myapplication.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.a45myapplication.R
+import com.example.a45myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
 }
